7675e90826a5223aa5d6e7a57d51eb7f370bb570
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-10 11:32:25 -0800

    WIP: writing out second test pondering what i need to write

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -25,5 +25,12 @@ namespace Bakery.Models
 
 /* Bread:Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.
 Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.
+you need to have a variable for bread at $5
+you need a start value of 0 to start the price off 
+you need buy 2x get on 1x free 
+3x - 2x= x 15 - 
+a method to 
+
+
 */
 
