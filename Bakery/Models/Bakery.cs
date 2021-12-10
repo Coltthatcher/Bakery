c5649e10837e9c1bbc403261d2391de8592f37bb
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-10 10:15:09 -0800

    write out pseudo code in order to plan project out

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 namespace Bakery.Models
 {
   public class Bread
@@ -18,3 +19,9 @@ namespace Bakery.Models
 
   }
 }
+
+
+
+/* Bread:Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.
+Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.
+*/
