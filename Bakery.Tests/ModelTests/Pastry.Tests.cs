e935b5ca5c34bde5b25e54637b18ef941b349514
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-10 13:38:57 -0800

    add pastry tests folder, write out first test in said folder

---
+++ Bakery.Tests/ModelTests/Pastry.Tests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Pastry.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+
+    [TestMethod]
+    public void PastryCount_CreatesInstanceOfItem_Item()
+    {
+      int pastryNumber = 0;
+      PastryCount newPastry = new PastryCount(pastryNumber);
+      int order = newPastry.PastryNumber;
+      Assert.AreEqual(pastryNumber, order);
+    }
+  }
+
+
+
+
+}
