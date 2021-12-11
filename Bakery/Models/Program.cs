baa8fe2ed043d02032aad219babc41a66f8aba9c
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-10 16:40:51 -0800

    craete first functionality in program.cs

--- Bakery/Models/Program.cs
+++ Bakery/Models/Program.cs
@@ -10,8 +10,8 @@ public class Program
     public static void Main()
     {
       Console.WriteLine( "Welcome! This is my little french bakery and I am excited to announce our special discount for today! Bread is always $5 while if you buy two the third is free, pastries are each $2 and a deal of $5 if you purchase 3 and so on! Enter 'B' for bread and/or 'P' for the pastries" );
-      Console.ReadLine().ToUpper();
-      if ("BP" || "PB")
+      String order = Console.ReadLine().ToUpper();
+      if ( order =="BP" || order == "PB")
       {
         Console.WriteLine("How much bread would you like to purchase?");
         int Loaf = int.Parse(Console.ReadLine());
@@ -19,7 +19,10 @@ public class Program
         int Pastry = int.Parse(Console.ReadLine());
         Console.WriteLine("The order total is!");
         PastryCount newPastryCount = new PastryCount(Pastry);
-        int PastryResult = PastryCount.Calculations(newPastryCount)
+        int PastryResult = PastryCount.Calculations(newPastryCount.PastryNumber);
+        BreadCount newBreadCount = new BreadCount(Loaf);
+        int BreadResult = BreadCount.Calculations(newBreadCount.BreadNumber);
+        Console.WriteLine(BreadResult+ PastryResult);
 
 
       }
