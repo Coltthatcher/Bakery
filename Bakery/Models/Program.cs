5e64bd15eaefdb2b59e83a362e6dece34158c631
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-10 16:09:02 -0800

    WIP: program cs file

--- Bakery/Models/Program.cs
+++ Bakery/Models/Program.cs
@@ -2,10 +2,28 @@ using System;
 using System.Collections.Generic;
 using Bakery.Models;  //import namespaces from Item.cs
 
-class Program
+
+namespace Bakery
 {
-  static void Main()
+public class Program
   {
-    
+    public static void Main()
+    {
+      Console.WriteLine( "Welcome! This is my little french bakery and I am excited to announce our special discount for today! Bread is always $5 while if you buy two the third is free, pastries are each $2 and a deal of $5 if you purchase 3 and so on! Enter 'B' for bread and/or 'P' for the pastries" );
+      Console.ReadLine().ToUpper();
+      if ("BP" || "PB")
+      {
+        Console.WriteLine("How much bread would you like to purchase?");
+        int Loaf = int.Parse(Console.ReadLine());
+        Console.WriteLine("How many pastries?");
+        int Pastry = int.Parse(Console.ReadLine());
+        Console.WriteLine("The order total is!");
+        PastryCount newPastryCount = new PastryCount(Pastry);
+        
+
+      }
+      
+
+    }
   }
 }
